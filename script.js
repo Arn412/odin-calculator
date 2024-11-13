30483fcd4f1d44d7e1e15f7a0112ce5c8bf6138c
Arn412 <122379262+Arn412@users.noreply.github.com>
2024-11-13 22:38:10 +0530

    finished display functionality

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
 const display = document.getElementById("#display")
 
-let num1, num2, operator;
+let num1, num2, operator, curNum = "", second = false;
 
+function appendNum(n) {
+    curNum += n
+    if (second) {
+        updateDisplay(num1 + operator + curNum)
+    }
+    else {
+        updateDisplay(curNum)
+    }
+}
 
 function updateDisplay(n) {
     document.getElementById("display").value = n;
+}
+
+function clearDisplay() {
+    num1 = ""
+    num2 = ""
+    operator = ""
+    curNum = ""
+    second = false
+    document.getElementById("display").value = "";
+}
+
+function setOperator(o) {
+    switch (o) {
+        case "add":
+            operator = "+"
+            break;
+        case "sub":
+            operator = "-";
+            break;
+        case "mult":
+            operator = "*";
+            break;
+        case "div":
+            operator = "/";
+            break;
+    }
+    console.log(operator)
+    num1 = parseInt(curNum);
+    curNum = "";
+    second = true;
+    updateDisplay(num1 + operator)
+
 }
 
 function add(x, y) {
